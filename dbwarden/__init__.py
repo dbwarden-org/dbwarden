fa4d96fcd27b95a371aefd4e5408272dfa4be9ba
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-09-13 16:21:11 -0300

    feat: export register_migration_hooks

--- dbwarden/__init__.py
+++ dbwarden/__init__.py
@@ -1,11 +1,11 @@
 from importlib.metadata import version
 
-from dbwarden.config_registry import DbwardenDatabase, database_config
+from dbwarden.config_registry import DbwardenConfig, DbwardenDatabase, database_config
 from dbwarden.databases.clickhouse import ChEngineSpec, CHTableMeta
 from dbwarden.databases.mysql import MyColumnMeta, MyTableMeta
 from dbwarden.databases.pgsql import PGViewMeta
 from dbwarden.databases.sqlite import SqColumnMeta, SqTableMeta
-from dbwarden.plugin import load_plugins
+from dbwarden.plugin import load_plugins, register_migration_hooks
 from dbwarden.seed import Seed, SeedRow, seed_data
 
 __version__ = version("dbwarden")
@@ -13,6 +13,7 @@ __version__ = version("dbwarden")
 __all__ = [
     "__version__",
     "database_config",
+    "DbwardenConfig",
     "DbwardenDatabase",
     "ChEngineSpec",
     "CHTableMeta",
@@ -22,6 +23,7 @@ __all__ = [
     "SqColumnMeta",
     "SqTableMeta",
     "load_plugins",
+    "register_migration_hooks",
     "Seed",
     "SeedRow",
     "seed_data",
